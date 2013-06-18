0a3749577de3e870b9cb29cd0f7d3c2d52f8cd8a
Hunter Bridges <hbridges@gmail.com>
2013-06-17 23:02:29 -0700

    Mac library

---
+++ liblua-mac/liblua_mac.h
@@ -0,0 +1,13 @@
+//
+//  liblua_mac.h
+//  liblua-mac
+//
+//  Created by Hunter Bridges on 6/17/13.
+//  Copyright (c) 2013 妖道. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface liblua_mac : NSObject
+
+@end
---
+++ liblua-mac/liblua_mac.m
@@ -0,0 +1,13 @@
+//
+//  liblua_mac.m
+//  liblua-mac
+//
+//  Created by Hunter Bridges on 6/17/13.
+//  Copyright (c) 2013 妖道. All rights reserved.
+//
+
+#import "liblua_mac.h"
+
+@implementation liblua_mac
+
+@end
